6acc6568469577c15db3ededb692c11a7155eab8
Pedro Castanheira <josepedrocastanheira@gmail.com>
2016-12-13 11:37:46 +0000

    added fizzbuzz ruby file and ammended require on fizzbuzz spec file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,8 @@
 # RSpec is one of the most used Behaviour Driven Development tools in the ruby community.
 # https://github.com/makersacademy/pre_course/blob/master/pills/rspec.md
 
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     # eq - A matcher is essentially a method that will match the operation inside the
