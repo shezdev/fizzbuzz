2f9a4f9eed821c0b005bbe3b6ffae59cde1347e4
Shereen <shereenfinney@gmail.com>
2016-12-13 11:50:05 +0000

    Saved extra comments in fizzbuzz_spec file

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,11 @@
 # RSpec is one of the most used Behaviour Driven Development tools in the ruby community.
 # https://github.com/makersacademy/pre_course/blob/master/pills/rspec.md
 
+# RSpec actually adds lib to its LOAD_PATH by default. This means that it will
+# look for required files in lib automatically, which is why we can simply
+# require 'fizzbuzz'. Ruby will infer the .rb extension if it is omitted,
+# so this is optional too.
+
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
