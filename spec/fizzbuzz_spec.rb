33ac9c642f70ce1c9db2dcb9a17b418d3c56a8e2
Shereen <shereenfinney@gmail.com>
2016-12-13 11:18:59 +0000

    1st Test - failed

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,10 @@
+# RSpec is one of the most used Behaviour Driven Development tools in the ruby community.
+# https://github.com/makersacademy/pre_course/blob/master/pills/rspec.md
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    # eq - A matcher is essentially a method that will match the operation inside the
+    # expect method (result) with the expectation (fizzbuzz(3)= fizz).
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
